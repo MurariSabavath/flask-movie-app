131d42e3754c0e84302b4ba28bb38175388fd636
Murari Sabavath <46033399+MurariSabavath@users.noreply.github.com>
2021-03-29 13:07:25 +0530

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,10 +1,6 @@
 import os
 class Config():
-    # API_KEY = os.environ.get("API_KEY")
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    # SECRET_KEY = os.environ.get("SECRET_KEY")
-    # URL = os.environ.get("URL")
-    API_KEY = 'a6d6836dce41469572be86049d892465'
-    SECRET_KEY = 'eb9493fa643a22204c3a3f1cc73ec3e2ca265550769a4085'
+    API_KEY = os.environ.get("API_KEY")
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    URL = 'https://api.themoviedb.org/3/'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    URL = os.environ.get("URL")
