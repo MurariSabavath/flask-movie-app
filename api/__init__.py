7c350ca8053a2fdcd61eadf9f0dd062bdfeb1b49
Murari Sabavath <smurari1004@gmail.com>
2021-03-29 12:48:58 +0530

    Made few changes

--- api/__init__.py
+++ api/__init__.py
@@ -3,9 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from .config import Config
-from dotenv import load_dotenv
 
-load_dotenv()
+
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
