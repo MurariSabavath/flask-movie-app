96b92918e2afce9206e3f65561cc9239c3685032
Murari Sabavath <46033399+MurariSabavath@users.noreply.github.com>
2021-03-30 21:06:43 +0530

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from .config import Config
+# from dotenv import load_dotenv
+
+# load_dotenv()
 
 
 db = SQLAlchemy()
