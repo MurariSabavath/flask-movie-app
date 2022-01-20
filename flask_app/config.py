cbc47256bb413ac805ab7de3a63aceeb69566c1f
MurariSabavath <smurari1004@gmail.com>
2022-01-20 22:27:49 +0530

    added database_url

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,6 +1,6 @@
 import os
 class Config():
     API_KEY = os.environ.get("API_KEY")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     SECRET_KEY = os.environ.get("SECRET_KEY")
     URL = os.environ.get("URL")
