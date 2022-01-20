0c867bac9e2bbcf100fd1f0ee6e3f5bb3650a548
MurariSabavath <smurari1004@gmail.com>
2022-01-20 22:54:21 +0530

    update config

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,6 +1,14 @@
 import os
+
+def database_uri_replace():
+    uri = os.environ.get('DATABASE_URL')
+    if uri and uri.startswith("postgres://"):
+        return uri.replace("postgres://", "postgresql://", 1)
+    return uri
+
 class Config():
     API_KEY = os.environ.get("API_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
+    SQLALCHEMY_DATABASE_URI = database_uri_replace()
     SECRET_KEY = os.environ.get("SECRET_KEY")
     URL = os.environ.get("URL")
+
